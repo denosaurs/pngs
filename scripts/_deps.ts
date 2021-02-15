4c536c88ff57e8f970e532762a29bb7b8ee2c03d
eliassjogreen <eliassjogreen1@gmail.com>
2021-02-15 04:15:07 +0100

    chore: bump deps

--- scripts/_deps.ts
+++ scripts/_deps.ts
@@ -1,4 +1,4 @@
-export { encode } from "https://deno.land/std@0.86.0/encoding/base64.ts";
-export { exists } from "https://deno.land/std@0.86.0/fs/mod.ts";
+export { encode } from "https://deno.land/std@0.87.0/encoding/base64.ts";
+export { exists } from "https://deno.land/std@0.87.0/fs/mod.ts";
 export { compress } from "https://deno.land/x/lz4@v0.1.2/mod.ts";
 export { minify } from "https://jspm.dev/terser@5.5.1";
