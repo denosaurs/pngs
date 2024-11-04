13723a104d253af0e13fa58641b8e8825411b150
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-11-04 14:15:26 -0500

    chore: fmt + lint

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,6 +1,6 @@
 import { decode } from "../mod.ts";
 
 const file = await (await fetch(
-    "https://avatars.githubusercontent.com/u/65427464?s=49&v=4",
+  "https://avatars.githubusercontent.com/u/65427464?s=49&v=4",
 )).bytes();
 console.log(decode(file));
